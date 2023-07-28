2550b5e583f4e08c8d0bdb6f061a8c556a985c90
Yann Figueiredo <yann.fabricio@hotmail.com>
2023-07-28 14:37:53 -0300

    refactor: movidos app e global style para um componente provider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { GlobalStyle } from './styles/GlobalStyle';
+import AppProviders from './providers/AppProviders';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <AppProviders />
   </React.StrictMode>
 );
---
+++ frontend/src/providers/AppProviders.js
@@ -0,0 +1,11 @@
+import App from "../App";
+import { GlobalStyle } from "../styles/GlobalStyle";
+
+export default function AppProviders() {
+    return(
+        <>
+            <GlobalStyle />
+            <App />
+        </>
+    )
+}
