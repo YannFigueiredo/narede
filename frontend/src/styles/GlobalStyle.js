e1c2ff31330ea0f29fc8913d71e7cb16f8bc9657
Yann Figueiredo <yann.fabricio@hotmail.com>
2023-07-28 18:08:17 -0300

    feat: adicionadas fontes padrões

--- frontend/src/styles/GlobalStyle.js
+++ frontend/src/styles/GlobalStyle.js
@@ -5,5 +5,13 @@ export const GlobalStyle = createGlobalStyle`
         padding: 0;
         margin: 0;
         box-sizing: border-box;
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        color: #000;
+    }
+
+    h1, h2, h3 {
+      font-family: "Comic Neue", sans-serif;
+      font-weight: 700;
     }
 `;
