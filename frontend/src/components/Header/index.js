4f83cd1ab53e2e959b9d994ac4319258c2f298c0
Yann Figueiredo <yann.fabricio@hotmail.com>
2023-08-17 11:19:40 -0300

    style: adicionada versão mobile do menu do header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Container, ImageWrapper, MenuWrapper, Menu } from "./styles";
 import Logo from "assets/images/logo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import CancelIcon from "@mui/icons-material/Cancel";
 import LoginIcon from "@mui/icons-material/Login";
 
 export default function Header() {
+  const menu = useRef(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const openMenu = () => {
+    setIsMenuOpen(true);
+    menu.current.style.display = "flex";
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+    menu.current.style.display = "none";
+  };
+
+  useEffect(() => {
+    if(isMenuOpen) {
+      global.document.body.style.overflow = "hidden";
+      global.document.documentElement.style.overflow = "hidden";
+    } else {
+      global.document.body.style.overflow = "auto";
+      global.document.documentElement.style.overflow = "auto";
+    }
+  }, [isMenuOpen]);
+
   return(
     <Container>
       <ImageWrapper href="/">
         <img src={Logo} alt="Logo do site Nave HQ"/>
       </ImageWrapper>
       <MenuWrapper>
-        <Menu>
+        <Menu ref={menu}>
           <li><a href="/biblioteca">Biblioteca</a></li>
           <li><a href="#">Leitor</a></li>
           <li><a href="#">Produtor</a></li>
           <li><a href="#">Blog</a></li>
           <li><a href="#">Sobre</a></li>
+          <CancelIcon id="menu-close" onClick={closeMenu} />
         </Menu>
+        <MenuIcon id="menu-open" onClick={openMenu} />
         <LoginIcon />
       </MenuWrapper>
     </Container>
--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -26,7 +26,7 @@ export const MenuWrapper = styled.nav`
   gap: ${({theme}) => theme.spacing.between_elements.normal};
 
   svg {
-    font-size: 28px;
+    font-size: ${({theme}) => theme.fontSize.large};
     fill: rgba(255, 255, 255, .7);
     cursor: pointer;
     transition: all linear .3s;
@@ -35,7 +35,34 @@ export const MenuWrapper = styled.nav`
   svg:hover {
     fill: rgba(255, 255, 255, 1);
   }
-`;
+
+  #menu-open {
+    display: none;
+    font-size: ${({theme}) => theme.fontSize.large};
+  }
+
+  #menu-close {
+    display: none;
+    position: absolute;
+    top: 0;
+    right: 0;
+    fill: rgba(255, 255, 255, .7);
+    z-index: 999;
+    font-size: ${({theme}) => theme.fontSize.very_large};
+    cursor: pointer;
+    margin: ${({theme}) => theme.spacing.padding.normal} ${({theme}) => theme.spacing.padding.normal} 0 0;
+  }
+
+  @media screen and (max-width: 600px) {
+    #menu-open {
+      display: inline-block;
+    }
+
+    #menu-close {
+      display: inline-block;
+    }
+  }
+ `;
 
 export const Menu = styled.ul`
   display: flex;
@@ -58,5 +85,29 @@ export const Menu = styled.ul`
 
   li:hover {
     background: linear-gradient(to right, ${({theme}) => theme.colors.primaryGradient}, ${({theme}) => theme.colors.secondaryGradient});
+  }
+
+  @media screen and (max-width: 600px) {
+    display: none;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: ${({theme}) => theme.colors.primary};
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    z-index: 999;
+    padding-top: 3.5rem;
+
+    li {
+      width: 100%;
+      text-align: center;
+    }
+
+    li a {
+      font-size: ${({theme}) => theme.fontSize.large};
+    }
   }
 `;
