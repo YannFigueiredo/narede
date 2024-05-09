bc8310f4893a09bd8eb8299c6f25292bf65651d2
YannFigueiredo <yann.fabricio@hotmail.com>
2024-05-09 20:51:20 -0300

    refactor: adjusted structure

--- frontend/src/components/Input/index.js
+++ frontend/src/components/Input/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, InputWrapper, SearchButton } from "./styles";
+import { Container, Main, InputWrapper, SearchButton } from "./styles";
 import SearchIcon from "@mui/icons-material/Search";
 
 export default function Input({
@@ -12,18 +12,21 @@ export default function Input({
   onKeyDown
 }) {
   return(
-    <Container width={width} isSearch={isSearch} className={className}>
-      <InputWrapper 
-        isSearch={isSearch}
-        placeholder={placeholder}
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-      />
-      {isSearch &&
-        <SearchButton>
-          <SearchIcon onClick={onClick} />
-        </SearchButton>
-      }
+    <Container width={width} isSearch={isSearch}>
+      <Main className={className}>
+        <InputWrapper 
+          isSearch={isSearch}
+          placeholder={placeholder}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          className="input-wrapper"
+        />
+        {isSearch &&
+          <SearchButton className="search-button">
+            <SearchIcon onClick={onClick} />
+          </SearchButton>
+        }
+      </Main>
     </Container>
   );
 }
