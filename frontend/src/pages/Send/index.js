140ddd940ff60bbee9d3fe07403f71e81ec4a1c1
YannFigueiredo <yann.fabricio@hotmail.com>
2024-05-11 19:12:41 -0300

    refactor: send page modified

--- frontend/src/pages/Send/index.js
+++ frontend/src/pages/Send/index.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { Container } from "components/Container/page.js";
 import {
+  Container,
   Main,
   SubmitWrapper,
   ButtonWrapper
 } from "./styles.js";
 import SubmitImage from "assets/images/submissao-img.png";
-import SubmitText from "assets/images/submissao-texto.png";
 
 export default function Send() {
   return(
     <Container>
       <Main>
+        <h2>Submissão</h2>
         <SubmitWrapper>
           <img src={SubmitImage} alt="Imagem da seção de submissão" />
-          <img src={SubmitText} alt="Texto da seção de submissão" />
         </SubmitWrapper>
         <ButtonWrapper>
           <a href="#">Regras de submissão e envio</a>
         </ButtonWrapper>
+        <div className="bg"></div>
       </Main>
     </Container>
   );
