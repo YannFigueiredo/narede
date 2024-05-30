eb56f602f7a38a9c82660f6a09ec4ac573b27c15
YannFigueiredo <yann.fabricio@hotmail.com>
2024-05-29 21:14:01 -0300

    style: add header mobile queries

--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -19,10 +19,11 @@ export const Container = styled.header`
 
   @media screen and (max-width: 768px) {
     padding: ${({theme}) => theme.spacing.padding.small};
-  }
-
-  @media screen and (max-width: 768px) {
     background: rgb(56, 52, 53);
+
+    .show-search {
+      display: flex;
+    }
   }
 
   @media screen and (max-width: 600px) {
@@ -67,100 +68,27 @@ export const ImageWrapper = styled.div`
   }
 `;
 
-export const OptionsWrapper = styled.div`
-  width: 100%;
-`;
-
-export const ActionWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: ${({theme}) => theme.spacing.between_elements.between_sections};
-
-  @media screen and (max-width: 768px) {
-    flex-direction: column;
-    gap: ${({theme}) => theme.spacing.between_elements.between_cards};
-  }
-`;
-
 export const MenuWrapper = styled.nav`
   display: flex;
   align-items: center;
 
-  #menu-open {
-    display: none;
-    font-size: ${({theme}) => theme.fontSize.large};
-    cursor: pointer;
-  }
-
-  #menu-close {
-    display: none;
-    position: absolute;
-    top: 0;
-    right: 0;
-    fill: rgba(255, 255, 255, .7);
-    z-index: 9999;
-    font-size: ${({theme}) => theme.fontSize.very_large};
-    cursor: pointer;
-    margin: ${({theme}) => theme.spacing.padding.normal} ${({theme}) => theme.spacing.padding.normal} 0 0;
-  }
-
-  @media screen and (max-width: 992px) {
+  @media screen and (max-width: 768px) {
     #menu-open {
-      display: inline-block;
+      display: none;
     }
 
     #menu-close {
-      display: inline-block;
-    }
-
-    #main-menu {
-    li a {
-      font-size: ${({theme}) => theme.fontSize.regular};
-    }
-  }
-
-  #main-menu li a:hover, #main-menu li .active {
-    color: ${({theme}) => theme.colors.detailPrimary};
-  }
-
-
-    #main-menu {
       display: none;
-      position: fixed;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background: ${({theme}) => theme.colors.primary};
-      flex-direction: column;
-      justify-content: flex-start;
-      align-items: center;
-      z-index: 9999;
-      padding-top: 3.5rem;
-  
-      li {
-        width: 100%;
-        text-align: center;
-      }
-  
-      li a {
-        font-size: ${({theme}) => theme.fontSize.base_large};
-      }
     }
   }
-
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
- `;
+`;
 
 export const Menu = styled.ul`
   display: flex;
   justify-content: center;
   align-items: center;
   text-transform: uppercase;
-  width: 100%;
+  width: 100vw;
   background: radial-gradient(circle at center, #FFF, ${({theme}) => theme.colors.backgroundPrimary});
 
   li img {
@@ -212,8 +140,21 @@ export const Menu = styled.ul`
     max-width: 2rem;
   }
 
+  @media screen and (max-width: 1278px) {
+    background: none;
+    flex-wrap: wrap;
+  }
+
   @media screen and (max-width: 768px) {
+    background: none;
+    width: 100%;
+    gap: ${({theme}) => theme.spacing.between_elements.normal};
+
     li {
+      display: none;
+    }
+    
+    .mobile-option {
       display: flex;
       flex-direction: column;
       align-items: center;
@@ -221,86 +162,35 @@ export const Menu = styled.ul`
       gap: ${({theme}) => theme.spacing.between_elements.small};
     }
 
-    li img {
+    .mobile-option a {
+      transition: all linear .3s;
+      color: #FFF;
+      background-color: transparent;
+      font-size: 0.5rem;
+      font-weight: 700;
+      padding: 0;
+    }
+
+    .mobile-option .menu-icon {
+      display: inline-block;
+      font-size: 2.3rem;
+      margin-bottom: -0.2rem;
+    }
+
+    .mobile-option a:hover, .mobile-option .active {
+      color: #FFF;
+      background-color: ${({theme}) => theme.colors.backgroundPrimary};
+      font-weight: 700;
+    }
+
+    .mobile-option img {
       display: inline-block;
     }
 
     #logged-container, #logged-out-container {
-      li a {
+      .mobile-option a {
         font-size: 0.5rem;
       }
-    }
-
-    li .menu-icon {
-      font-size: 40px;
-      display: inline-block;
-      width: 2rem;
-      height: 2rem;
-    }
-
-    #about-btn-mobile {
-      display: flex;
-    }
-  }
-`;
-
-export const SessionManager = styled.div`
-  li img {
-    display: none;
-    width: 2rem;
-    height: 2rem;
-  }
-
-  li .menu-icon {
-    display: none;
-  }
-
-  #about-btn-mobile {
-    display: none;
-  }
-
-  #logged-container {
-    li:nth-child(1) {
-      display: flex;
-      align-items: center;
-      gap: ${({theme}) => theme.spacing.between_elements.small};
-    }
-  }
-
-  #logged-container, #logged-out-container {
-    li {
-      padding: ${({theme}) => theme.spacing.padding.big_small} 0;
-    }
-
-    li a {
-      font-size: 1rem;
-    }
-  } 
-
-  @media screen and (max-width: 768px) {
-    li {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      gap: ${({theme}) => theme.spacing.between_elements.small};
-    }
-
-    li img {
-      display: inline-block;
-    }
-
-    #logged-container, #logged-out-container {
-      li a {
-        font-size: 0.5rem;
-      }
-    }
-
-    li .menu-icon {
-      font-size: 40px;
-      display: inline-block;
-      width: 2rem;
-      height: 2rem;
     }
 
     #about-btn-mobile {
@@ -315,6 +205,12 @@ export const InputWrapper = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   padding: 10px 20px;
+
+  @media screen and (max-width: 768px) {
+    background: none;
+    display: none;
+    padding: 0;
+  }
 `;
 
 export const InputBorder = styled.div`
@@ -323,4 +219,9 @@ export const InputBorder = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   padding: 10px 20px;
+  transition: all linear .4s;
+
+  @media screen and (max-width: 768px) {
+    background: none;
+  }
 `;
